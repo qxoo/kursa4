497ad12c12ededddb4d22cef4cb7f4afc6783288
qxoo <overlap123123@gmail.com>
2023-06-07 17:30:24 +0300

    danilo

--- graphics.js
+++ graphics.js
@@ -1,33 +1,43 @@
+let pageIsLoaded = false
+// let toggleBtn = null;
+let windowWidth = null
 class GraphicsMovement {
-	onPageLoaded() {
+	initializeCode() {
 		this.graphicsParagraph = document.getElementById("graphics")
-
+		this.toggleBtn = document.querySelector(".container_toggleBtn")
+		this.toggleBtn.addEventListener("click", () => this.animate())
 		this.timer = null
 		this.theta_sp = 0
 		this.phi_sp = 0
 	}
 	renderFrame() {
-		console.log("frame")
+		windowWidth = window.innerWidth
 		this.b = []
 		this.zBuffer = []
-		this.theta_sp += 0.07
-		this.phi_sp += 0.03
+		this.theta_sp += 0.05
+		this.phi_sp += 0.05
+		// console.log(this.theta_sp)
 		this.cosTheta_sp = Math.cos(this.theta_sp)
 		this.sinTheta_sp = Math.sin(this.theta_sp)
 		this.cosPhi_sp = Math.cos(this.phi_sp)
 		this.sinPhi_sp = Math.sin(this.phi_sp)
+		// console.log(this.sinPhi_sp)
+		// console.log(this.phi_sp)
 
-		for (let k = 0; k < 1760; k++) {
+		for (let k = 0; k <= windowWidth; k++) {
 			this.b[k] = k % 80 == 79 ? "\n" : " "
 			this.zBuffer[k] = 0
 		}
 		for (let theta = 0; theta < 6.28; theta += 0.07) {
-			this.cosTheta = Math.cos(this.theta)
-			this.sinTheta = Math.sin(this.theta)
+			this.cosTheta = Math.cos(theta)
+			this.sinTheta = Math.sin(theta)
+			//   console.log(theta)
+			//   console.log(this.cosTheta)
+			//   console.log(this.sinTheta)
 		}
 		for (let phi = 0; phi < 6.28; phi += 0.03) {
-			this.cosPhi = Math.cos(this.phi)
-			this.sinPhi = Math.sin(this.phi)
+			this.cosPhi = Math.cos(phi)
+			this.sinPhi = Math.sin(phi)
 			this.circleX = this.cosTheta + 2
 			this.z =
 				this.sinPhi +
@@ -72,19 +82,19 @@ class GraphicsMovement {
 				this.D > this.zBuffer[this.o]
 			) {
 				this.zBuffer[this.o] = this.D
-				this.b[this.o] = "!@#$%^&*()"[this.N > 0 ? this.N : 0]
+				this.b[this.o] = "@@%%##**++==--::..  "[this.N > 0 ? this.N : 0]
 			}
 		}
-		this.graphicsParagraph.innerHTML = this.b.join("")
-		// console.log(this.graphicsParagraph.innerHTML)
+		this.graphicsParagraph.innerHTML = this.b.join(" ")
+		// this.graphicsParagraph.innerHTML = 123
 	}
 	animate() {
-		// console.log(123)
 		if (this.timer === null) {
-			console.log(222)
-			this.timer = setInterval(this.renderFrame(), 50)
-			// setInterval(this.renderFrame(), 5)
+			console.log("animate true")
+			this.timer = setInterval(() => this.renderFrame(), 50)
 		} else {
+			console.log("animate false")
+
 			this.renderFrame()
 			clearInterval(this.timer)
 			this.timer = null
@@ -92,14 +102,5 @@ class GraphicsMovement {
 	}
 }
 const moving = new GraphicsMovement()
-const toggleBtn = document.querySelector(".container_toggleBtn")
-moving.onPageLoaded()
-const asd = document.getElementById("button_toggle")
-asd.addEventListener("click", () => moving.animate())
-// toggleBtn.addEventListener("click", moving.animate())
-// console.log(12412412)
-window.addEventListener("load", moving.onPageLoaded, false)
 
-// window.onload = function(){
-
-// }
+window.addEventListener("load", moving.initializeCode())
